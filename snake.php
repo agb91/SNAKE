d8e0755b390a6aab9e0cf7a62ad7fc9db35f8ecb
agb91 <andrea.damio1991@hotmail.it>
2015-05-26 13:12:22 +0200

    Create snake.php

---
+++ snake.php
@@ -0,0 +1,28 @@
+<?php 
+require_once ("controllo.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Gioca a Snake</title>
+        
+    </head>
+    <body onload = "avanza();">
+        <h1>Welcome <span id="nomeUtente"><?php echo $_SESSION["login"]?></span></h1>
+        <p><a href="index.php">Home</a></p>
+        <table style="width:500px" style="height:300px">
+        <tr>
+        <td style="text-align:left">Punteggio: <span id="punteggio" type=text>0</span></td>
+        <td style="text-align:right">Livello: <span id="livello" type=text>1</span></td>
+        </tr>
+        <tr>
+        <td id="gioco" colspan="2" style="text-align:center">
+        <script type="text/javascript" src="snake.js"></script>
+        </td></tr>
+        </table>
+        
+        
+        
+        
+    </body>
+</html>
