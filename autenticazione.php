510ed8fdbd2e1de4ca1f9ee4a55a3324a757f5e8
agb91 <andrea.damio1991@hotmail.it>
2015-05-26 13:07:33 +0200

    Create autenticazione.php

---
+++ autenticazione.php
@@ -0,0 +1,41 @@
+<?php 
+require_once ("talkToData.php"); //da query sul database
+//require_once ("config.php");
+
+
+	session_start();	
+	if (count($_POST)==2) //chiamato da login
+	{
+		if (talkToData::verificaUtente($_POST["username"], $_POST["password"])) 
+		//if ($nomeUtente==$_POST["username"] && $passwordUtente==$_POST["password"])
+		{
+			// in questo caso, l'utente ha effettuato il login correttamente
+			$_SESSION["login"] = $_POST["username"];
+			header("location: index.php");
+		}
+		else 
+		{
+			header("location: login.php?errore=0");
+		}
+	}
+	else //chiamato da registrazione
+	{
+			if ($_POST["password"]!=$_POST["password2"]) //se le due password non coincidono
+			{
+				header("location: registrazione.php?errore=0");
+			}
+			else 
+			{
+				if (talkToData::inserisciUtente($_POST["username"], $_POST["password"])) //la registrazione � andata a buon fine
+				//if ($nomeUtente!=$_POST["username"])
+				{
+					$_SESSION["login"] = $_POST["username"];
+					header("location: index.php");
+				}
+				else //nome utente gi� presente
+				{
+					header("location: registrazione.php?errore=1");
+				}
+			}
+	}
+?>
