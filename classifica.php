b83d6d65ffa59645beedafee8f00c3487e14ef56
agb91 <andrea.damio1991@hotmail.it>
2015-05-26 13:08:19 +0200

    Create classifica.php

---
+++ classifica.php
@@ -0,0 +1,38 @@
+<?php 
+require_once ("controllo.php");
+?>
+<html>     
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <title>Ricezione classifica</title>
+    <style type="text/css">
+      * { font-size: 18pt; }   
+    </style>
+  </head>
+  <body>
+      
+    <div>
+     <fieldset>
+      <legend> <strong>classifica </strong></legend>
+        <?php  // L'INTESTATURA HTML SERVE PER QUESTIONI ESTETICHE (LA APRTE STYLE)
+          function __autoload($class_name) {        // non richiamato
+          require_once $class_name . '.php';
+          }
+          $utente = $_GET["utente"];
+          $punteggio = $_GET["punteggio"];
+          if($punteggio!=1)
+          {
+            talkToData::salvaRecord($utente,$punteggio);    // prova a scrivere nel caso di nuovo utente ed a scovrascrivere se utente gi� in db
+            talkToData::salvaTutto($utente,$punteggio);
+            echo "Salve ".$utente." il tuo punteggio &egrave;: " . $punteggio. "<br><br>" ;
+            echo "Il tuo record personale &egrave;: ". talkToData::leggiRecordPersonale($utente)."<br><br>";
+          }
+          echo "Il record generale &egrave;: " . talkToData::leggiRecordGenerale();
+          echo "<br><br>";
+          echo '<a href="logout.php">Esci</a><br>'
+        ?>
+     </fieldset>
+    </div>
+
+  </body>
+</html>
