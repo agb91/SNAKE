5844cf71c5fa42b1b2c0a80f57f68696672653de
pm89 <pm89@users.noreply.github.com>
2015-05-28 21:08:48 +0200

    Update cibo.js

--- cibo.js
+++ cibo.js
@@ -11,5 +11,74 @@ function creaCibo()
 	while (attuale != coloreSfondo)
 	cibo[0] = x;
 	cibo[1] = y;
-	colore(x, y, coloreCibo);
+	tipo=scegliCibo();
+	colore(x, y, tipo);
 }
+
+
+function scegliCibo()
+{
+	t=Math.random();
+	if (t<soglia)
+		return coloreCibo;
+	else
+		{
+			q=Math.floor(Math.random() * 3);
+			switch (q) 
+			{
+			case 0:
+				return coloreCiboBonus1;
+			case 1:
+				return coloreCiboBonus2;
+			case 2:
+				return coloreCiboBonus3;
+			}
+		}
+}
+
+
+function mangiaCibo()
+{
+	tipo=document.getElementById(cibo[0]+"-"+cibo[1]).style.backgroundColor;
+	switch (tipo) 
+	{
+	case coloreCibo: //cibo normale
+		{
+			punteggio = punteggio + incrementoPunteggio;
+			document.getElementById("punteggio").firstChild.nodeValue = punteggio;
+			break;
+		}
+	case coloreCiboBonus1: //bonus punti
+		{
+			punteggio = punteggio + incrementoPunteggio*3;
+			document.getElementById("punteggio").firstChild.nodeValue = punteggio;
+			break;
+		}
+	case coloreCiboBonus2: //serpente non aumenta di lunghezza
+		{
+			punteggio = punteggio + incrementoPunteggio;
+			document.getElementById("punteggio").firstChild.nodeValue = punteggio;
+			vettoreX.pop();
+			vettoreY.pop();
+			break;
+		}
+	case coloreCiboBonus3: //immortalità finchè non mangio altro
+		{
+			punteggio = punteggio + incrementoPunteggio*10;
+			document.getElementById("punteggio").firstChild.nodeValue = punteggio;
+			immortal=true;
+			break;
+		}
+	}
+}
+
+
+function isCibo(x,y)
+{
+	attuale=document.getElementById(x+"-"+y).style.backgroundColor;
+	if (attuale==coloreCibo || attuale==coloreCiboBonus1 || attuale==coloreCiboBonus2 || attuale==coloreCiboBonus3)
+		return true;
+	else 
+		return false;
+}
+
