bbe9da256db5b0be67dffbe843ed9c0d5e9cd0da
pm89 <pm89@users.noreply.github.com>
2015-05-28 13:00:08 +0200

    Update login.php

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
 	    <form method="post" action="autenticazione.php">
 	    	<table>
 	    	<tr><td>Username:</td><td><input type="text" name="username" size="10"/></td></tr>
-	    	<tr><td>Password:</td><td><input type="text" name="password" size="10"/></td></tr>
+	    	<tr><td>Password:</td><td><input type="password" name="password" size="10"/></td></tr>
 	    	</table>
 	    	<input type="submit" value="Accedi"/>
 	    </form>
