9f05d27142dc91dc1720648b8f61a70c9cd0ca6d
pm89 <pm89@users.noreply.github.com>
2015-06-01 16:33:04 +0200

    Update snake.php

--- snake.php
+++ snake.php
@@ -10,18 +10,31 @@ require_once ("controllo.php");
     <body onload = "avanza();">
         <h2>Welcome <span id="nomeUtente"><?php echo $_SESSION["login"]?></span></h2>
         <p><a href="index.php">Home</a></p>
-        <table style="width:500px" style="height:300px">
-        <tr>
-        <td style="text-align:left">Punteggio: <span id="punteggio" type=text>0</span></td>
-        <td style="text-align:right">Livello: <span id="livello" type=text>1</span></td>
-        </tr>
-        <tr>
-        <td id="gioco" colspan="2" style="text-align:center">
-        <script type="text/javascript" src="configuratore.js"></script>
-        <script type="text/javascript" src="cibo.js"></script>
-        <script type="text/javascript" src="labirinto.js"></script>
-        <script type="text/javascript" src="snake.js"></script>
-        </td></tr>
-        </table>     
+        <div style="float:left">
+	        <table style="width:500px" style="height:300px">
+		        <tr>
+			        <td style="text-align:left">Punteggio: <span id="punteggio" type=text>0</span></td>
+			        <td style="text-align:right">Livello: <span id="livello" type=text>1</span></td>
+		        </tr>
+		        <tr>
+			        <td id="gioco" colspan="2" style="text-align:center">
+			        <script type="text/javascript" src="configuratore.js"></script>
+			        <script type="text/javascript" src="cibo.js"></script>
+			        <script type="text/javascript" src="labirinto.js"></script>
+			        <script type="text/javascript" src="snake.js"></script>
+		        	</td>
+		        </tr>
+	        </table>
+        </div>
+        <div style="float:right">
+        	<table>ciao
+        		<thead>Legenda colori</thead>
+        		<tr><td id="c1"></td><td id="s1"></td></tr>
+        		<tr><td id="c2"></td><td id="s2"></td></tr>
+        		<tr><td id="c3"></td><td id="s3"></td></tr>
+        		<tr><td id="c4"></td><td id="s4"></td></tr>
+        	</table>    
+        </div>
+        
     </body>
 </html>
