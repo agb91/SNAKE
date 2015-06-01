fd34a5c82f08e42d67025f8e1416a3fd3b10c16f
pm89 <pm89@users.noreply.github.com>
2015-06-01 15:16:30 +0200

    Update registrazione.php

--- registrazione.php
+++ registrazione.php
@@ -15,9 +15,9 @@
     <p><a href="index.php">Home</a></p>
 	    <form method="post" action="autenticazione.php">
 	    	<table>
-	    	<tr><td>Username:</td><td><input type="text" name="username" size="10"/></td></tr>
-	    	<tr><td>Password:</td><td><input type="password" name="password" size="10"/></td></tr>
-	    	<tr><td>Ripeti password:</td><td><input type="password" name="password2" size="10"/></td></tr>
+	    	<tr><td>Username:</td><td><input type="text" name="username" size="12"/></td></tr>
+	    	<tr><td>Password:</td><td><input type="password" name="password" size="12"/></td></tr>
+	    	<tr><td>Ripeti password:</td><td><input type="password" name="password2" size="12"/></td></tr>
 	    	</table>
 	    	<input type="submit" value="Registrati"/>
 	    </form>
