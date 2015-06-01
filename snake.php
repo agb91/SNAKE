4959295474a6158845618dc49b3aa22963e46853
pm89 <pm89@users.noreply.github.com>
2015-06-01 17:20:31 +0200

    Update snake.php

--- snake.php
+++ snake.php
@@ -17,24 +17,22 @@ require_once ("controllo.php");
 			        <td style="text-align:right">Livello: <span id="livello" type=text>1</span></td>
 		        </tr>
 		        <tr>
-			        <td id="gioco" colspan="2" style="text-align:center">
-			        <script type="text/javascript" src="configuratore.js"></script>
-			        <script type="text/javascript" src="cibo.js"></script>
-			        <script type="text/javascript" src="labirinto.js"></script>
-			        <script type="text/javascript" src="snake.js"></script>
-		        	</td>
+			        <td id="gioco" colspan="2" style="text-align:center"></td>
 		        </tr>
 	        </table>
         </div>
-        <div style="float:right">
-        	<table>ciao
-        		<thead>Legenda colori</thead>
-        		<tr><td id="c1"></td><td id="s1"></td></tr>
-        		<tr><td id="c2"></td><td id="s2"></td></tr>
-        		<tr><td id="c3"></td><td id="s3"></td></tr>
-        		<tr><td id="c4"></td><td id="s4"></td></tr>
+        <div style="margin-left:600px">
+        	<table border="1" cellpadding="2" cellspacing="0">
+        		<tr colspan="2">Legenda colori</tr>
+        		<tr><td id="c1">c1</td><td id="s1">s4</td></tr>
+        		<tr><td id="c2">c2</td><td id="s2">s3</td></tr>
+        		<tr><td id="c3">c3</td><td id="s3">s2</td></tr>
+        		<tr><td id="c4">c4</td><td id="s4">s1</td></tr>
         	</table>    
         </div>
-        
+		<script type="text/javascript" src="configuratore.js"></script>
+		<script type="text/javascript" src="cibo.js"></script>
+		<script type="text/javascript" src="labirinto.js"></script>
+		<script type="text/javascript" src="snake.js"></script> 
     </body>
 </html>
