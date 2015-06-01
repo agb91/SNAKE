b856bbe03a3dc796e93f284bb8639c7e3766de08
agb91 <andrea.damio1991@hotmail.it>
2015-06-01 18:00:00 +0200

    Update labirinto.php

--- labirinto.php
+++ labirinto.php
@@ -1,4 +1,5 @@
-<?php
+<?php // INTERMEDIARIO CHE RICEVE LA RICHIESTA AJAX E LA GIRA A TALKTODATA CHE GUARDANDO NEL DB TROVERA LA RISPOSTA
+// POI RESTITUITA AL CLIENT USANDO ECHO (stesso schema es ajax fatto in classe)
     require_once "talkToData.php";
     $liv = $_GET["liv"];
     $ris = talkToData::ostacolo($liv);
