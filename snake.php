739eea54a09143963425dd3a6332bf47dc20c468
pm89 <pm89@users.noreply.github.com>
2015-06-17 11:12:35 +0200

    Update snake.php

--- snake.php
+++ snake.php
@@ -22,17 +22,14 @@ require_once ("controllo.php");
 	        </table>
         </div>
         <div style="margin-left:600px">
-        	<table border="1" cellpadding="2" cellspacing="0">
+        	<table id="legenda" border="1" cellpadding="2" cellspacing="0">
         		<tr colspan="2">Legenda colori</tr>
-        		<tr><td id="c1">c1</td><td id="s1">s4</td></tr>
-        		<tr><td id="c2">c2</td><td id="s2">s3</td></tr>
-        		<tr><td id="c3">c3</td><td id="s3">s2</td></tr>
-        		<tr><td id="c4">c4</td><td id="s4">s1</td></tr>
         	</table>    
         </div>
 		<script type="text/javascript" src="configuratore.js"></script>
 		<script type="text/javascript" src="cibo.js"></script>
 		<script type="text/javascript" src="labirinto.js"></script>
 		<script type="text/javascript" src="snake.js"></script> 
+		<?php $_SESSION["giocato"]=true ?>
     </body>
 </html>
